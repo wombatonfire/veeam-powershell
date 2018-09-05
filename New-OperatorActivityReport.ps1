ab3776240ae5433d905cd1ebbc652e32156ea4e8
Yuri Sukhov <yuri.sukhov@gmail.com>
2018-09-06 00:14:11 +0300

    Add New-OperatorActivityReport.ps1

---
+++ New-OperatorActivityReport.ps1
@@ -0,0 +1,274 @@
+param(
+    [ValidateCount(2, 2)]
+    [datetime[]]
+    $DateRange,
+
+    [string]
+    $Operator
+)
+
+$vboServer = ""
+$vboPort = "4443"
+
+$username = ""
+$password = ""
+
+$reportPath = ""
+
+function Get-AccessToken
+{
+    [CmdletBinding()]
+
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Server,
+        [string]
+        $Port = "4443",
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Username,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Password
+    )
+    $uri = "https://${Server}:$Port/v2/token"
+    $requestBody = "grant_type=password&username=$Username&password=$Password"
+    $response = Invoke-RestMethod -Uri $uri -Method Post -Body $requestBody
+
+    return $response.access_token
+}
+
+function Get-Resource
+{
+    [CmdletBinding()]
+
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Server,
+
+        [string]
+        $Port = "4443",
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Resource,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $AccessToken
+    )
+
+    $baseUri = "https://${Server}:$Port/v2"
+    $resourceUri = "$baseUri$Resource"
+    $headers = @{
+        Authorization = "Bearer $AccessToken"
+    }
+    $response = Invoke-RestMethod -Uri $resourceUri -Headers $headers
+
+    return $response
+}
+
+function Get-RestoreSessions
+{
+    [CmdletBinding()]
+
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Server,
+
+        [string]
+        $Port = "4443",
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $AccessToken,
+
+        [ValidateCount(2, 2)]
+        [datetime[]]
+        $DateRange,
+
+        [string]
+        $Operator
+    )
+    $restoreSessions = New-Object -TypeName System.Collections.Generic.List[PSCustomObject]
+
+    $response = Get-Resource -Server $Server -Port $Port -Resource "/RestoreSessions" -AccessToken $AccessToken
+    $restoreSessions.AddRange([System.Collections.Generic.List[PSCustomObject]]$response.results)
+    if ($response._links.next)
+    {
+        do
+        {
+            $offset = $response.offset + $response.limit
+            $resource = "/RestoreSessions?offset=$offset&limit=$($response.limit)"
+            $response = Get-Resource -Server $Server -Port $Port -Resource $resource -AccessToken $AccessToken
+            $restoreSessions.AddRange([System.Collections.Generic.List[PSCustomObject]]$response.results)
+        }
+        while ($response._links.next)
+    }
+
+    if ($DateRange)
+    {
+        $dateRangeUtc = $DateRange | ForEach-Object -Process {$_.ToUniversalTime()}
+        $sessionFilter = {
+            $creationTimeUtc = ([datetime]$_.creationTime).ToUniversalTime()
+            $creationTimeUtc -ge $dateRangeUtc[0] -and $creationTimeUtc -lt $dateRangeUtc[1]
+        }
+        $restoreSessions = $restoreSessions | Where-Object -FilterScript $sessionFilter
+    }
+    if ($Operator)
+    {
+        $restoreSessions = $restoreSessions | Where-Object -FilterScript {$_.initiatedBy -eq $Operator}
+    }
+    return $restoreSessions | Sort-Object -Property creationTime
+}
+
+function Get-RestoreSessionEvents
+{
+    [CmdletBinding()]
+
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Server,
+
+        [string]
+        $Port = "4443",
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $SessionId,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $AccessToken
+    )
+
+    $sessionEvents = New-Object -TypeName System.Collections.Generic.List[PSCustomObject]
+    $response = Get-Resource -Server $Server -Port $Port -Resource "/RestoreSessions/$SessionId/Events" `
+        -AccessToken $AccessToken
+    $sessionEvents.AddRange([System.Collections.Generic.List[PSCustomObject]]$response.results)
+    if ($response._links.next)
+    {
+        do
+        {
+            $offset = $response.offset + $response.limit
+            $resource = "/RestoreSessions/$SessionId/Events?offset=$offset&limit=$($response.limit)"
+            $response = Get-Resource -Server $Server -Port $Port -Resource $resource -AccessToken $AccessToken
+            $sessionEvents.AddRange([System.Collections.Generic.List[PSCustomObject]]$response.results)
+        }
+        while ($response._links.next)
+    }
+
+    return $sessionEvents
+}
+
+function ConvertFrom-EventMessageString
+{
+    [CmdletBinding()]
+
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]
+        $EventMessage
+    )
+
+    $itemNamePattern = [regex]"(?<=^Item ).*(?= was \w+ successfully \(type:)"
+    $itemTypePattern = [regex]"(?<=\(type: ).*(?=; source:)"
+    $sourcePattern = [regex]"(?<=source: ).*(?=; target:)"
+    $targetPattern = [regex]"(?<=target: ).*(?=\)$)"
+
+    $itemDetails = [PSCustomObject]@{
+        itemName = [regex]::Match($EventMessage, $itemNamePattern);
+        itemType = [regex]::Match($EventMessage, $itemTypePattern);
+        source = [regex]::Match($EventMessage, $sourcePattern);
+        target = [regex]::Match($EventMessage, $targetPattern)
+    }
+    return $itemDetails
+}
+
+function New-OperatorActivityReport
+{
+    [CmdletBinding()]
+
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Server,
+
+        [string]
+        $Port = "4443",
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Username,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Password,
+
+        [ValidateCount(2, 2)]
+        [datetime[]]
+        $DateRange,
+
+        [string]
+        $Operator
+    )
+
+    $report = New-Object -TypeName System.Collections.Generic.List[PSCustomObject]
+
+    $accessToken = Get-AccessToken -Server $Server -Port $Port -Username $Username -Password $Password
+    $filters = @{}
+    if ($DateRange)
+    {
+        $filters["DateRange"] = $DateRange
+    }
+    if ($Operator)
+    {
+        $filters["Operator"] = $Operator
+    }
+    $restoreSessions = Get-RestoreSessions -Server $Server -Port $Port -AccessToken $accessToken @filters
+    foreach ($session in $restoreSessions)
+    {
+        $sessionEvents = Get-RestoreSessionEvents -Server $Server -Port $Port -SessionId $session.id `
+            -AccessToken $accessToken
+        foreach ($event in $sessionEvents)
+        {
+            # Exclude restore session started/completed events
+            if ($event.type -ne "None")
+            {
+                $itemDetails = ConvertFrom-EventMessageString -EventMessage $event.message
+                $eventReport = [PSCustomObject]@{
+                    timeUtc = ([datetime]$event.startTime).ToUniversalTime();
+                    operator = $session.initiatedBy;
+                    organization = $session.organization;
+                    eventType = $event.type;
+                    itemName = $itemDetails.itemName;
+                    itemType = $itemDetails.itemType;
+                    source = $itemDetails.source;
+                    target = $itemDetails.target;
+                    status = $event.status
+                }
+                $report.Add($eventReport)
+            }
+        }
+    }
+
+    return $report
+}
+
+$filters = @{}
+if ($DateRange)
+{
+    $filters["DateRange"] = $DateRange
+}
+if ($Operator)
+{
+    $filters["Operator"] = $Operator
+}
+$report = New-OperatorActivityReport -Server $vboServer -Port $vboPort -Username $username -Password $password @filters
+$report | Export-Csv -Path $reportPath -NoTypeInformation
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # veeam-powershell
-PowerShell scripts for Veeam Backup &amp; Replication
 
-## New-CloudBinReport.ps1
+
+### New-CloudBinReport.ps1
 The script generates a report for Cloud Connect Backup tenants, showing the size of the deleted backups in a recycle bin.
 
 Backups deleted from a cloud repository are moved to the recycle bin if the service provider enabled *Insider Protection* option for the tenant. Files in the recycle bin do not consume the tenant quota, and this report provides insight into additional backup storage consumption.
 
-## New-OrgBackupReport.ps1
+### New-OrgBackupReport.ps1
 The script generates a backup usage report for vCloud Director organizations. For each organization the report provides:
 
 * The total number of VMs in backups.
@@ -15,3 +15,44 @@ The script generates a backup usage report for vCloud Director organizations. Fo
 Note that `$protectedVms` is a simple sum of all VMs in all backups, so if a particular VM is processed by multiple jobs it will be counted multiple times.
 
 **IMPORTANT**: First version of this script used jobs to find the backups, thus the backups without a job were not counted. Current version uses quota to locate the backups, so that all backups are proccessed, even the ones without a corresponding job. Please upgrade if you use previous version.
+
+## Scripts for Veeam Backup for Microsoft Office 365
+
+### New-OperatorActivityReport.ps1
+The script generates an operator activity report, showing which application items a restore operator had access to during a restore session. The report contains the following fields:
+
+* Time of the event in UTC.
+* Operator login.
+* Organization name for which a restore session was initiated.
+* Event type. Possible types: *Save*, *Export*, *Send*, *Restore*, *View*.
+* Item name.
+* Item type.
+* Source path of the item.
+* Target path of the item (if applicable).
+* Event status.
+
+Events can be filtered by providing a date range in the `-DateRange` parameter and/or operator login in the `-Operator` parameter. By default events from all available restore sessions are included.
+
+#### Execution
+
+The script is implemented in PowerShell, but it uses REST API for interacting with the backup server, thus PowerShell cmdlets for Veeam Backup for Microsoft Office 365 are not required.
+
+1. Specify the values for the following variables in the script:
+
+
+2. Save the script.
+3. Run the script from the PowerShell console, providing optional `-DateRange` and/or `-Operator` parameters if needed.
+
+For example:
+
+    PS C:\Users\Administrator> C:\PSScripts\New-OperatorActivityReport.ps1
+    PS C:\Users\Administrator> C:\PSScripts\New-OperatorActivityReport.ps1 -DateRange 2018-08-01, 2018-09-01
+    PS C:\Users\Administrator> C:\PSScripts\New-OperatorActivityReport.ps1 -DateRange "2018-09-01 15:00", "2018-09-01 18:00"
+    PS C:\Users\Administrator> C:\PSScripts\New-OperatorActivityReport.ps1 -Operator "domain\username"
+    PS C:\Users\Administrator> C:\PSScripts\New-OperatorActivityReport.ps1 -DateRange 2018-08-01, 2018-09-01 -Operator "domain\username"
+
+If date is specified without time, midnight is assumed. If time is provided, it is assumed to be in local time zone. Start timestamp is included in the range, end timestamp is not.
+
+Generated report in a csv format will be saved to the file specified in the `$reportPath` variable.
+
+Note that, while date range for the script should be provided in local time zone, timestamps in the report are in UTC. This is done to avoid ambiguity and support data exchange when report users are located in different time zones.
