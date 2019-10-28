e4765fae8601530fa8b6386847c8918a043ee896
Yuri Sukhov <yuri.sukhov@gmail.com>
2019-10-29 02:47:26 +0300

    New-OrgBackupReport v2
    Add support for non-self-service vCD backups.
    Add support for aggregation on the organization vDC level.
    Improve documentation.

--- New-OrgBackupReport/New-OrgBackupReport.ps1
+++ New-OrgBackupReport/New-OrgBackupReport.ps1
@@ -1,8 +1,58 @@
+$reportPath = ""
+
 Add-PSSnapin -Name VeeamPSSnapin
+
+function Get-VcdVAppLocation
+{
+    [CmdletBinding()]
+
+    param(
+        [Parameter(Mandatory=$true)]
+        [Veeam.Backup.Core.CBackup]
+        $Backup,
+
+        [Parameter(Mandatory=$true,
+        ParameterSetName="VM")]
+        [guid]
+        $VMObjectId,
+
+        [Parameter(Mandatory=$true,
+        ParameterSetName="vApp")]
+        [guid]
+        $VAppObjectId
+    )
+
+    if ($PSCmdlet.ParameterSetName -eq "VM")
+    {
+        $vmOib = $Backup.FindLastOib($VMObjectId)
+        $vAppOib = $vmOib.FindParent()
+    }
+    elseif ($PSCmdlet.ParameterSetName -eq "vApp")
+    {
+        $vAppOib = $Backup.FindLastOib($VAppObjectId)
+    }
+
+    return $vAppOib.AuxData.OrigVApp.VCloudVAppLocation
+}
 
 function New-OrgBackupReport
 {
-    $report = New-Object -TypeName System.Collections.Generic.List[PSCustomObject]
+    [CmdletBinding()]
+
+    param(
+        [switch]
+        $AggregateByOrgVdc,
+
+        [switch]
+        $IncludeAllVcdBackups
+    )
+
+    if ($IncludeAllVcdBackups)
+    {
+        $selfServiceBackupIds = New-Object -TypeName System.Collections.Generic.List[guid]
+    }
+
+    $orgReports = @{}
 
     $vcdOrgItems = Find-VBRvCloudEntity | Where-Object -FilterScript {$_.Type -eq "Organization"}
     foreach ($item in $vcdOrgItems)
@@ -12,33 +62,175 @@ function New-OrgBackupReport
         $orgQuotaId = [Veeam.Backup.Core.CJobQuota]::FindByOrganization($vcdOrg).Id
         if ($orgQuotaId)
         {
-            $protectedVms = 0
-            $usedSpace = 0
-
             $orgBackupIds = [Veeam.Backup.DBManager.CDBManager]::Instance.Backups.FindBackupsByQuotaIds($orgQuotaId).Id
-            foreach ($backupId in $orgBackupIds)
+            if ($AggregateByOrgVdc)
             {
-                $backup = [Veeam.Backup.Core.CBackup]::Get($backupId)
-                $protectedVms += ($backup.GetObjects() | Where-Object -FilterScript {$_.Type -eq "VM"}).Length
-                $sizePerStorage = $backup.GetAllStorages().Stats.BackupSize
-                foreach ($size in $sizePerStorage)
+                foreach ($backupId in $orgBackupIds)
                 {
-                    $usedSpace += $size
+                    if ($IncludeAllVcdBackups)
+                    {
+                        $selfServiceBackupIds.Add($backupId)
+                    }
+
+                    $backup = [Veeam.Backup.Core.CBackup]::Get($backupId)
+                    $storages = $backup.GetAllStorages()
+                    foreach ($object in $backup.GetObjects())
+                    {
+                        if ($object.Type -eq "VM")
+                        {
+                            $vcdVAppLocation = Get-VcdVAppLocation -Backup $backup -VMObjectId $object.Id
+                        }
+                        elseif ($object.Type -eq "NfcDir")
+                        {
+                            $vcdVAppLocation = Get-VcdVAppLocation -Backup $backup -VAppObjectId $object.Id
+                        }
+                        $orgVdcName = $vcdVAppLocation.OrgVdcName
+                        if (!$orgReports.Contains($vcdOrg.OrgName))
+                        {
+                            $orgReports[$vcdOrg.OrgName] = @{}
+                        }
+                        if (!$orgReports[$vcdOrg.OrgName].Contains($orgVdcName))
+                        {
+                            $orgReports[$vcdOrg.OrgName][$orgVdcName] = [PSCustomObject]@{
+                                protectedVms = 0;
+                                usedSpace = 0
+                            }
+                        }
+                        if ($object.Type -eq "VM")
+                        {
+                            $orgReports[$vcdOrg.OrgName][$orgVdcName].protectedVms += 1
+                        }
+                        $sizePerObjectStorage = ($storages | Where-Object -FilterScript {$_.ObjectId -eq $object.Id}).Stats.BackupSize
+                        foreach ($size in $sizePerObjectStorage)
+                        {
+                            $orgReports[$vcdOrg.OrgName][$orgVdcName].usedSpace += $size
+                        }
+                    }
                 }
             }
+            else
+            {
+                foreach ($backupId in $orgBackupIds)
+                {
+                    if ($IncludeAllVcdBackups)
+                    {
+                        $selfServiceBackupIds.Add($backupId)
+                    }
 
-            $orgReport = [PSCustomObject]@{
-                orgName = $vcdOrg.OrgName;
-                protectedVms = $protectedVms;
-                usedSpace = $usedSpace
+                    $backup = [Veeam.Backup.Core.CBackup]::Get($backupId)
+                    if (!$orgReports.Contains($vcdOrg.OrgName))
+                    {
+                        $orgReports[$vcdOrg.OrgName] = [PSCustomObject]@{
+                            protectedVms = 0;
+                            usedSpace = 0
+                        }
+                    }
+                    $orgReports[$vcdOrg.OrgName].protectedVms += ($backup.GetObjects() | Where-Object -FilterScript {$_.Type -eq "VM"}).Length
+                    $sizePerStorage = $backup.GetAllStorages().Stats.BackupSize
+                    foreach ($size in $sizePerStorage)
+                    {
+                        $orgReports[$vcdOrg.OrgName].usedSpace += $size
+                    }
+                }
             }
-            $report.Add($orgReport)
+        }
+    }
+
+    if ($IncludeAllVcdBackups)
+    {
+        $allVcdBackups = Get-VBRBackup | Where-Object -FilterScript {$_.BackupPlatform.ToString() -eq "EVcd"}
+        $nonSelfServiceVcdBackups = $allVcdBackups | Where-Object -FilterScript {$_.Id -notin $selfServiceBackupIds}
+        foreach ($backup in $nonSelfServiceVcdBackups)
+        {
+            $storages = $backup.GetAllStorages()
+            foreach ($object in $backup.GetObjects())
+            {
+                if ($object.Type -eq "VM")
+                {
+                    $vcdVAppLocation = Get-VcdVAppLocation -Backup $backup -VMObjectId $object.Id
+                }
+                elseif ($object.Type -eq "NfcDir")
+                {
+                    $vcdVAppLocation = Get-VcdVAppLocation -Backup $backup -VAppObjectId $object.Id
+                }
+                $orgName = $vcdVAppLocation.OrgName
+                if ($AggregateByOrgVdc)
+                {
+                    $orgVdcName = $vcdVAppLocation.OrgVdcName
+                    if (!$orgReports.Contains($orgName))
+                    {
+                        $orgReports[$orgName] = @{}
+                    }
+                    if (!$orgReports[$orgName].Contains($orgVdcName))
+                    {
+                        $orgReports[$orgName][$orgVdcName] = [PSCustomObject]@{
+                            protectedVms = 0;
+                            usedSpace = 0
+                        }
+                    }
+                    if ($object.Type -eq "VM")
+                    {
+                        $orgReports[$orgName][$orgVdcName].protectedVms += 1
+                    }
+                    $sizePerObjectStorage = ($storages | Where-Object -FilterScript {$_.ObjectId -eq $object.Id}).Stats.BackupSize
+                    foreach ($size in $sizePerObjectStorage)
+                    {
+                        $orgReports[$orgName][$orgVdcName].usedSpace += $size
+                    }
+                }
+                else
+                {
+                    if (!$orgReports.Contains($orgName))
+                    {
+                        $orgReports[$orgName] = [PSCustomObject]@{
+                            protectedVms = 0;
+                            usedSpace = 0
+                        }
+                    }
+                    if ($object.Type -eq "VM")
+                    {
+                        $orgReports[$orgName].protectedVms += 1
+                    }
+                    $sizePerObjectStorage = ($storages | Where-Object -FilterScript {$_.ObjectId -eq $object.Id}).Stats.BackupSize
+                    foreach ($size in $sizePerObjectStorage)
+                    {
+                        $orgReports[$orgName].usedSpace += $size
+                    }
+                }
+            }
+        }
+    }
+
+    $report = New-Object -TypeName System.Collections.Generic.List[PSCustomObject]
+
+    if ($AggregateByOrgVdc)
+    {
+        foreach ($orgReportEntry in $orgReports.GetEnumerator())
+        {
+            foreach ($orgVdcReportEntry in $orgReportEntry.Value.GetEnumerator())
+            {
+                $report.Add([PSCustomObject]@{
+                    orgName = $orgReportEntry.Key;
+                    orgVdcName = $orgVdcReportEntry.Key;
+                    protectedVms = $orgVdcReportEntry.Value.protectedVms
+                    usedSpace = $orgVdcReportEntry.Value.usedSpace
+                })
+            }
+        }
+    }
+    else
+    {
+        foreach ($orgReportEntry in $orgReports.GetEnumerator())
+        {
+            $report.Add([PSCustomObject]@{
+                orgName = $orgReportEntry.Key;
+                protectedVms = $orgReportEntry.Value.protectedVms;
+                usedSpace = $orgReportEntry.Value.usedSpace
+            })
         }
     }
 
     return $report
 }
 
-New-OrgBackupReport | Format-Table -Property @{Expression={$_.orgName}; Label="Organization"},
-    @{Expression={$_.protectedVms}; Label="Protected VMs"},
-    @{Expression={[System.Math]::Round($_.usedSpace / 1GB, 2)}; Label="Used space (GB)"}
+New-OrgBackupReport | Export-Csv -Path $reportPath -NoTypeInformation
--- New-OrgBackupReport/README.md
+++ New-OrgBackupReport/README.md
@@ -6,4 +6,44 @@ The script generates a backup usage report for vCloud Director organizations. Fo
 
 Note that `$protectedVms` is a simple sum of all VMs in all backups, so if a particular VM is processed by multiple jobs it will be counted multiple times.
 
-**IMPORTANT**: First version of this script used jobs to find the backups, thus the backups without a job were not counted. Current version uses quota to locate the backups, so that all backups are proccessed, even the ones without a corresponding job. Please upgrade if you use previous version.
+The usage data can be aggregated on the organization level or for individual organization vDCs.
+
+The backup scope of the report is customizable. It can be limited to the self-service backups, created by the organization administrators via self-service backup portal, or it can also include the backups, managed by the provider and created directly on the backup server.
+
+### How to use
+
+1. Specify a path to an output CSV file in the `$reportPath` variable on the first line of the script.
+2. If necessary, add the configuration parameters for the `New-OrgBackupReport` command on the last line of the script. See the [Details](#details) section for parameter description.
+3. Run the script. The report will be saved in the CSV file.
+
+### Details
+
+The central part of the script is the `New-OrgBackupReport` function. By default it's called without parameters, which creates a report for self-service backups with aggregation on the organization level.
+
+To change the aggregation level and/or backup scope of the report, two additional parameters can be provided for the `New-OrgBackupReport` function:
+
+- `-AggregateByOrgVdc` - a switch parameter used to enable data aggregation on the organization vDC level. Useful when backups for different organization vDCs are billed differently.
+- `-IncludeAllVcdBackups` - a switch parameter used to include usage statistics for all vCD backups. Backups, managed by the provider and created directly on the backup server, are included in the report and attributed to the correct organization.
+
+`-AggregateByOrgVdc` and `-IncludeAllVcdBackups` parameters are independent and can be used together or on their own.
+
+**IMPORTANT**: `-AggregateByOrgVdc` and `-IncludeAllVcdBackups` parameters REQUIRE the backups to be stored in per-VM backup files ("Use per-VM backup files" option in advanced settings of a backup repository). This is bacause the size of the backups is calculated using backup files, and, when a backup file contains several VMs from different organizations/vDCs, it's impossible to reliably attribute per-VM consumption to the correct organization/vDC.
+
+The `usedSpace` column in the report is in bytes. You might want to convert it to GB or TB. This can be achieved by using `Select-Object` cmdlet between `New-OrgBackupReport` and `Export-Csv` on the last line of the script, e.g.:
+
+Convert the `usedSpace` to GB, round the value to 2 decimal places and pretty-print the column names for a report with aggregation per organization (3 columns):
+
+```powershell
+New-OrgBackupReport | Select-Object -Property @{Expression={$_.orgName}; Label="Organization"},
+    @{Expression={$_.protectedVms}; Label="Protected VMs"},
+    @{Expression={[System.Math]::Round($_.usedSpace / 1GB, 2)}; Label="Used space (GB)"} | Export-Csv -Path $reportPath -NoTypeInformation
+```
+
+Convert the `usedSpace` to GB, round the value to 2 decimal places and pretty-print the column names for a report with aggregation per organization vDC (4 columns):
+
+```powershell
+New-OrgBackupReport | Select-Object -Property @{Expression={$_.orgName}; Label="Organization"},
+    @{Expression={$_.orgVdcName}; Label="vDC"},
+    @{Expression={$_.protectedVms}; Label="Protected VMs"},
+    @{Expression={[System.Math]::Round($_.usedSpace / 1GB, 2)}; Label="Used space (GB)"} | Export-Csv -Path $reportPath -NoTypeInformation
+```
